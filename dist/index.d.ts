74359971a4a7a9fdbc8465f8fe046189f2a8dc9f
Robert Orliński <kontakt@robertorlinski.pl>
2022-01-10 23:09:12 +0100

    Update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -9,6 +9,7 @@ export declare type Giscus = {
 	theme?: Theme;
 	reactionsEnabled?: BooleanString;
 	emitMetadata?: BooleanString;
+	lang?: Lang;
 };
 export declare type BooleanString = "0" | "1";
 export declare type Session = {
@@ -17,6 +18,7 @@ export declare type Session = {
 export declare type Repo = `${string}/${string}`;
 export declare type Mapping = "url" | "title" | "og:title" | "specific" | "number" | "pathname";
 export declare type Theme = "light" | "dark" | "dark_dimmed" | "dark_high_contrast" | "preferred_color_scheme" | "transparent_dark" | `https://${string}`;
-export function Giscus({ repo, repoId, category, categoryId, mapping, term, theme, reactionsEnabled, emitMetadata }: GiscusProps): JSX.Element | null;
+export declare type Lang = "en" | "fr" | "de" | "gsw" | "es" | "it" | "ja" | "ko" | "pl" | "id" | "ro" | "zh-CN" | "zh-TW" | string;
+export function Giscus({ repo, repoId, category, categoryId, mapping, term, theme, reactionsEnabled, emitMetadata, lang }: GiscusProps): JSX.Element | null;
 
 export {};
